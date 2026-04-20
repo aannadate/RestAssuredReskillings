4338df893a4df5ebc9f36f4151875e440a758b5b
aniket <annadateaniket@gmail.com>
2026-04-20 17:45:21 +0530

    Building e2e automation testing using get post patch

--- RestAssuredreskill/src/test/java/files/PayloadsExamples.java
+++ RestAssuredreskill/src/test/java/files/PayloadsExamples.java
@@ -1,24 +1,22 @@
 package files;
+
 public class PayloadsExamples {
 
+	public static String addPlaceJson() {
+		return "{\r\n" + "    \"location\": {\r\n" + "        \"lat\": -58.383494,\r\n"
+				+ "        \"lng\": 63.427362\r\n" + "    },\r\n" + "    \"accuracy\": 50,\r\n"
+				+ "    \"name\": \"Arihant Niwas \",\r\n" + "    \"phone_number\": \"(+91) 8999552006\",\r\n"
+				+ "    \"address\": \"Near Old balaji temple\",\r\n" + "    \"types\": [\r\n" + "        \"home\",\r\n"
+				+ "        \"shop\"\r\n" + "    ],\r\n" + "    \"website\": \"http://google.com\",\r\n"
+				+ "    \"language\": \"French-IN\"\r\n" + "}";
+
+	}
 	
-		public static String addPlaceJson() {
+	public static String updatePlace(String placeID) {
 		return "{\r\n"
-				+ "    \"location\": {\r\n"
-				+ "        \"lat\": -58.383494,\r\n"
-				+ "        \"lng\": 63.427362\r\n"
-				+ "    },\r\n"
-				+ "    \"accuracy\": 50,\r\n"
-				+ "    \"name\": \"Arihant Niwas \",\r\n"
-				+ "    \"phone_number\": \"(+91) 8999552006\",\r\n"
-				+ "    \"address\": \"Near Old balaji temple\",\r\n"
-				+ "    \"types\": [\r\n"
-				+ "        \"home\",\r\n"
-				+ "        \"shop\"\r\n"
-				+ "    ],\r\n"
-				+ "    \"website\": \"http://google.com\",\r\n"
-				+ "    \"language\": \"French-IN\"\r\n"
+				+ "    \"place_id\": \" " + placeID +" \",\r\n"
+				+ "    \"address\": \"70 Summer walk, USA\",\r\n"
+				+ "    \"key\": \"qaclick123\"\r\n"
 				+ "}";
-	
-}
+	}
 }
--- RestAssuredreskill/src/test/java/session4_6/DemoTest.java
+++ RestAssuredreskill/src/test/java/session4_6/DemoTest.java
@@ -12,31 +12,33 @@ public class DemoTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		RestAssured.baseURI = "https://rahulshettyacademy.com";
-		
+
+		// add place --> update place with new address --> get place to validate if new
+		// address is present in response
+
 		RestAssured.baseURI = "https://rahulshettyacademy.com";
 
-		String response = given()
-		        .log().all()
-		        .queryParam("key", "qaclick123")
-		        .header("Content-Type", "application/json")
-		        .body(PayloadsExamples.addPlaceJson())
-		.when()
-		        .post("maps/api/place/add/json")
-		.then()
-		        .log().all()
-		        .assertThat()
-		        .statusCode(200)
-		        .body("scope", equalTo("APP"))
-		        .header("server", "Apache/2.4.52 (Ubuntu)")
-		        .extract()
-		        .response()
-		        .asString();
-		
+		String response = given().log().all().queryParam("key", "qaclick123").header("Content-Type", "application/json")
+				.body(PayloadsExamples.addPlaceJson()).when().post("maps/api/place/add/json").then().log().all()
+				.assertThat().statusCode(200).body("scope", equalTo("APP")).header("server", "Apache/2.4.52 (Ubuntu)")
+				.extract().response().asString();
+
 		JsonPath js = new JsonPath(response);
 		String placeId = js.getString("place_id");
-		System.out.println("=========>"+placeId);
+		System.out.println("=========>" + placeId);
+
+		// update place
+
+		given().log().all().queryParam("key", "qaclick123").header("Content-Type", "application/json")
+				.body(PayloadsExamples.updatePlace(placeId)).when().put("/maps/api/place/update/json").then()
+				.assertThat().statusCode(200).body("msg", equalTo("Address successfully updated"));
+
+		// get address
+
+		String getPlaceResponse = given().log().all().queryParam("place_id", placeId).queryParam("key", "qaclick123").when()
+				.get("maps/api/place/get/json").then().assertThat().log().all().statusCode(200).extract().response().asString();
+		
+		JsonPath js1 = new JsonPath(getPlaceResponse);
 		
 
 	}
